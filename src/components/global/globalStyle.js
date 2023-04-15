42c8c53362bcdb59a17a91b9234a1243f76dc7dc
monoha724 <bunnyalex724@gmail.com>
2023-04-15 17:15:08 +0900

    tempSave : 메인페이지 기능 연결 및 CSS작업 중

--- src/components/global/globalStyle.js
+++ src/components/global/globalStyle.js
@@ -21,8 +21,8 @@ export const FlexDiv = styled.div`
 
 export const GridDiv = styled.div`
     display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    grid-gap: 2rem;
+    grid-template-columns: ${({gridTC}) => gridTC};
+    grid-gap: 1rem;
     flex-direction: ${({fDirection}) => fDirection ? 'column' : 'row'};
     justify-content : ${({jc}) => jc ? jc : 'start'};
     align-items: ${({alignItem}) => alignItem ? alignItem : 'start'};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,6 +18,20 @@ function Home() {
     location: ""
   });
 
+  const { data, refetch } = useQuery({
+    queryKey: ["GET_MAINPAGE"],
+    queryFn: async () => {
+      // const token = getCookie("token");
+      const res = await axios.get(`${process.env.REACT_APP_SERVER_URL}/main`)
+      console.log("res", res);
+      return res.data.data;
+    }
+  })
+
+  useEffect(()=>{
+    refetch()
+  },[searchData]);
+
   const styleOption = `
     margin: auto;
     display: flex;
@@ -53,9 +67,62 @@ function Home() {
             </Slide>
           </Div>
         </Div>
-        <Div 
-          marginTop="2rem"
-          onClick={()=>{navi("/search")}}>검색페이지</Div>
+        <button 
+          onClick={()=>{navi("/search")}}>검색페이지 이동버튼! 누르세요!
+        </button>
+
+        {/* 대여 물품 8개 영역*/}
+        <h2>새로 등록된 대여 물품을 확인해보세요!</h2>
+        <GridDiv gridTC="repeat(4, 1fr)">
+          {data?.productList?.map((item) => 
+            <Div fDirection="row" onClick={()=>{navi(`/detail/${item?.id}`)}}>
+              <CardImg src={item?.image} alt="" />
+              <span>{item?.title}</span>
+              <span>{item?.price}</span>
+              <span>{item?.location}</span>
+            </Div>
+          )}
+        </GridDiv>
+        <hr />
+
+        {/* 이벤트 및 쿠폰 이미지 3개 영역*/}
+        <h2>빌리지에서 드리는 더 큰 혜택</h2>
+        <Div width="100%" jc="space-between" gap="2rem">
+          <Div>
+            <img src="" alt="왼쪽 사각 큰 이미지" />
+          </Div>
+          <Div>
+            <Div>
+              <img src="" alt="오른쪽 위 이미지" />
+            </Div>
+            <Div>
+              <img src="" alt="오른쪽 아래 이미지" />
+            </Div>
+          </Div>
+        </Div>
+        <hr />
+        {/* 추천 물품 6개 영역*/}
+        <h2>오늘의 추천 상품을 확인해보세요</h2>
+        <GridDiv gridTC="repeat(2, 1fr)">
+          {data?.randomProduct?.map((item) => 
+            <Div gap="1rem" height="100%" border="1px solid #e6e6e6">
+              <CardImg src={item?.image} alt="" />
+              <Div fDirection="row" jc="space-between" width="100%" height="100%">
+                <Div fDirection="row">
+                  <span>{item?.title}</span>
+                  <span>{item?.location}</span>
+                </Div>
+                <Div width="100%" jc="space-between">
+                  <span>{item?.price}</span>
+                  {!item?.checkZzim
+                    ? <img src="/images/eHeart.png" alt="" style={{width:"2rem", height:"2rem"}}/>
+                    : <img src="/images/fHeart.png" alt="" style={{width:"2rem", height:"2rem"}}/>
+                  }
+                </Div>
+              </Div>
+            </Div>
+          )}
+        </GridDiv>
       </MaxWidthDiv>
     </FlexDiv>
   )
@@ -84,4 +151,9 @@ const Slide = styled.div`
 const Img = styled.img`
   width: 100%;
   object-fit: fill;
+`
+
+const CardImg = styled.img`
+  width: 100px;
+  height: 100px;
 `
